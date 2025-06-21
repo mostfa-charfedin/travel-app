5b1c5832d576c5097dd52cf88f94aae0632f65a9
mostfa-charfedin <akkezmostfacharfedin@gmail.com>
2025-06-21 01:44:41 +0100

    implement Routing, Dashboard layout + UI

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Outlet} from "react-router";
+import {SidebarComponent} from "@syncfusion/ej2-react-navigations";
+import NavItems from "../../../components/NavItems";
+import {MobileSidebar} from "../../../components";
 
 const AdminLayout = () => {
     return (
         <div className="admin-layout">
-            MobileSidebar
+            <MobileSidebar />
             <aside className="w-full max-w-[270px] hidden lg:block">
-                Side
+                <SidebarComponent width={270} enableGestures={false}>
+                    <NavItems />
+                </SidebarComponent>
             </aside>
             <aside className="children">
                 <Outlet />
